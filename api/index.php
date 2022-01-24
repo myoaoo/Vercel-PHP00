9d3186577f6b73bbf28900e086dd98a1c8a78af9
Rishi Pratap <rp0258@srmist.edu.in>
2022-01-25 02:33:13 +0530

    folder added1

--- api/index.php
+++ api/index.php
@@ -6,7 +6,7 @@
       //define ('SITE_ROOT', realpath(dirname(__FILE__)));
       //move_uploaded_file($_FILES['file']['tmp_name'], SITE_ROOT.'/static/images/slides/1/1.jpg');
       echo file_get_contents(__DIR__ . '/../src/users.json');
-      $target = __DIR__ . '/../images'.basename($_FILES['image']['name']);
+      $target = __DIR__ . '../images/'.basename($_FILES['image']['name']);
 
       //connect to the data base
       $db = mysqli_connect("sql6.freemysqlhosting.net","sql6467804","lsCL6PbqR9","sql6467804","3306");
